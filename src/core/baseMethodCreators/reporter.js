fb65ad902e0724888d4ae9ba7eff92aeb24be96f
robinlu <robinlu0705@gmail.com>
2017-10-19 12:13:31 +1100

    Adjust the report format

--- src/core/baseMethodCreators/reporter.js
+++ src/core/baseMethodCreators/reporter.js
@@ -4,7 +4,7 @@ export { REPORT as METHOD_NAME } from './constants';
 export default function () {
   return function report() {
     if (Utils.checkInside(this.state.x, this.state.y, this.state.tableSize)) {
-      console.log(`Output: ${this.state.x}, ${this.state.y}, ${this.state.f}`);
+      console.log(`Output: ${this.state.x},${this.state.y},${this.state.f}`);
     }
   };
 }
